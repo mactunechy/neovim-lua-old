1b58e349b831da4e2b7b915a024dc42260c42b43
Dellan Muchengapadare <mactunechy@gmail.com>
2023-10-14 21:05:12 +0200

    add oil and completion

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -21,6 +21,11 @@ return require('packer').startup(function(use)
   use 'nvim-treesitter/nvim-treesitter'
   use 'hrsh7th/nvim-cmp'
   use 'hrsh7th/cmp-nvim-lsp'
+  use 'L3MON4D3/LuaSnip'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'rafamadriz/friendly-snippets'
+
+  use 'stevearc/oil.nvim'
   use {
       'nvim-telescope/telescope.nvim',
       tag = '0.1.0',
